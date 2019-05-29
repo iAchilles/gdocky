3486eb7396c6d6d72ee1c0fb3c2ee657af257bd6
Igor Manturov, JR <igor.manturov.jr@gmail.com>
2019-05-29 10:36:17 -0400

    Added mutations route

--- app/routes/mutations.js
+++ app/routes/mutations.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+    renderTemplate(controller, model) {
+        let cntrl = this.controllerFor('mutations.index');
+        this.render('mutations/index-header-outlet', {
+            outlet: 'header-outlet',
+            controller: cntrl
+        });
+
+        this._super(controller, model);
+    }
 });
