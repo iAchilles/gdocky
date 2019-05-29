693a8f3369888fb6b96aa5ce5410c5e042157f78
Igor Manturov, JR <igor.manturov.jr@gmail.com>
2019-05-29 19:49:06 -0400

    Added renderTemplate() method implementation

--- app/routes/queries.js
+++ app/routes/queries.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+    renderTemplate(controller, model) {
+        let cntrl = this.controllerFor('queries.index');
+        this.render('queries/index-header-outlet', {
+            outlet: 'header-outlet',
+            controller: cntrl
+        });
+
+        this._super(controller, model);
+    }
 });
