eae81abea6ed3a4ba0fac0cf019216c7281ab361
Igor Manturov, JR <igor.manturov.jr@gmail.com>
2019-06-01 19:51:41 -0400

    Updated structure of types dictionary

--- app/dictionaries/types.js
+++ app/dictionaries/types.js
@@ -1,14 +1,50 @@
 
 const Types = {
-    Queries: 'queries.index',
-    Mutations: 'mutations.index',
-    Scalars: 'scalars.index',
-    Enums: 'enums.index',
-    'Input Objects': 'inputs.index',
-    Objects: 'objects.index',
-    Interfaces: 'interfaces.index',
-    Unions: 'unions.index',
-    Directives: 'directives.index'
+    QUERY: {
+        name: 'Queries',
+        indexRoute: 'queries.index',
+        itemRoute: null
+    },
+    MUTATION: {
+        name: 'Mutations',
+        indexRoute: 'mutations.index',
+        itemRoute: null
+    },
+    SCALAR: {
+        name: 'Scalars',
+        indexRoute: 'scalars.index',
+        itemRoute: 'scalars.scalar'
+    },
+    ENUM: {
+        name: 'Enums',
+        indexRoute: 'enums.index',
+        itemRoute: 'enums.enum'
+    },
+    INPUT_OBJECT: {
+        name: 'Input Objects',
+        indexRoute: 'inputs.index',
+        itemRoute: 'input.input'
+    },
+    OBJECT: {
+        name: 'Objects',
+        indexRoute: 'objects.index',
+        itemRoute: 'objects.object'
+    },
+    INTERFACE: {
+        name: 'Interfaces',
+        indexRoute: 'interfaces.index',
+        itemRoute: 'interfaces.interface'
+    },
+    UNION: {
+        name: 'Unions',
+        indexRoute: 'unions.index',
+        itemRoute: 'unions.union'
+    },
+    DIRECTIVE: {
+        name: 'Directives',
+        indexRoute: 'directives.index',
+        itemRoute: null
+    }
 }
 
 export default Types;
