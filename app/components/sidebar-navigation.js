9607a62ca75e374270460b173918396c4d8033d8
Igor Manturov, JR <igor.manturov.jr@gmail.com>
2019-06-01 20:38:48 -0400

    Sidebar navigation

--- app/components/sidebar-navigation.js
+++ app/components/sidebar-navigation.js
@@ -1,10 +1,14 @@
-import Component    from '@ember/component';
-import Types        from 'gdocky/dictionaries/types';
-import { computed } from '@ember/object';
+import Component             from '@ember/component';
+import Types                 from 'gdocky/dictionaries/types';
+import { computed }          from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+
+    router: service(),
 
     types: computed(function () {
         return Types;
     })
+
 });
