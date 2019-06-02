0c70119e99b16e8c60f742d98b60248b48b2ea09
Igor Manturov, JR <igor.manturov.jr@gmail.com>
2019-06-01 23:51:34 -0400

    Added browser language detection

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,22 +2,42 @@ import Route                        from '@ember/routing/route';
 import { get }                      from '@ember/object';
 import { inject as service }        from '@ember/service';
 import ENV                          from 'gdocky/config/environment';
-import { default as introspection } from 'gdocky/gql/queries/introspection';
+import fetch                        from 'fetch';
+import { introspectionQuery }       from 'graphql';
 
 export default Route.extend({
-
-    apollo: service(),
 
     intl: service(),
 
     beforeModel() {
         this._super(...arguments);
-        let lang = ENV.lang;
-        let intl = get(this, 'intl');
-        intl.setLocale([ lang ]);
+        let intl     = get(this, 'intl');
+        let detected = navigator.language || navigator.userLanguage;
+        detected     = detected.toLowerCase();
+        let language = (detected === 'ru-ru' || detected === 'en-us') ? detected : 'en-us';
+
+        return intl.setLocale([ language ]);
     },
 
+
     model() {
-        return get(this, 'apollo').query({ query: introspection });
+        const schemaUrl = ENV.schemaUrl;
+        const body      = {
+            operationName: 'IntrospectionQuery',
+            query: introspectionQuery
+        }
+
+        return fetch(schemaUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body)
+        })
+            .then(function (response) {
+                return response.json().then(function (response) {
+                   return get(response, 'data');
+                });
+            });
     }
 });
