d094b5f6d5dd37705342a19013482ec11e73ebe7
Igor Manturov, JR <igor.manturov.jr@gmail.com>
2019-06-04 08:49:13 -0400

    Added directives

--- app/routes/directives/index.js
+++ app/routes/directives/index.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+    model() {
+        return this.modelFor('application');
+    },
+
+    renderTemplate(controller, model) {
+        this.render('directives/index-header-outlet', {
+            outlet: 'header-outlet',
+            controller: controller,
+            into: 'application'
+        });
+
+        this._super(controller, model);
+    }
+
 });
