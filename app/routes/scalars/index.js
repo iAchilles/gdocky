a3f32fd6b6a60a91b5d95cf7bba308ba623bdd74
Igor Manturov, JR <igor.manturov.jr@gmail.com>
2019-06-04 09:54:40 -0400

    Added scalars

--- app/routes/scalars/index.js
+++ app/routes/scalars/index.js
@@ -1,4 +1,20 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+    model() {
+        return this.modelFor('application');
+    },
+
+    renderTemplate(controller, model) {
+
+        this.render('objects/index-header-outlet', {
+            into: 'application',
+            outlet: 'header-outlet',
+            controller: controller
+        });
+
+        this._super(controller, model);
+    }
+
 });
